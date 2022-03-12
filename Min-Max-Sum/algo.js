5edc8a09e09c2e0ef09fdd41da87136ae284fc21
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-03-12 11:16:56 -0800

    add first test

--- Min-Max-Sum/algo.js
+++ Min-Max-Sum/algo.js
@@ -9,14 +9,12 @@ function miniMaxSum(arr) {
     for (let x=0; x<4; x++) {
         min += sArr[x];
     }
-    console.log("Min", min);
 
     let max=0;
     for (let x=1; x<5; x++) {
-        man += sArr[x];
+        max += sArr[x];
     }
-    console.log("Max", max);
+    console.log(min,max);
 }
 
-console.log("*******************");
-console.log( "miniMaxSum( [1,2,3,4,5] )", miniMaxSum( [1,2,3,4,5] ) );
+miniMaxSum( [1,3,5,7,9] );
