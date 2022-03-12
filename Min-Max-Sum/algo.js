08c35b1fe3a1c4f1c16ac25ccf84ed0facdcca2d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-03-12 11:08:49 -0800

    find min in minmaxsum

--- Min-Max-Sum/algo.js
+++ Min-Max-Sum/algo.js
@@ -5,6 +5,11 @@
 function miniMaxSum(arr) {
     let sArr = arr.sort();          // Create a sorted array
 
+    let min=0
+    for (let x=0; x<4; x++) {
+        min += arr[x];
+    }
+    console.log("Min", min);
 }
 
 console.log("*******************");
